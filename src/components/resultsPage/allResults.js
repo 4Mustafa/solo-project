746af161f97ce57dc187ed40cac557af1b761f77
mustafaibrahim4 <4MustafaIbrahim@gmail.com>
2020-04-29 21:46:30 -0500

    all results layout complete

---
+++ src/components/resultsPage/allResults.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+
+class allResult extends Component {
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_ALLRESULTS' });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <header> ALL RESULTS</header>
+                {this.props.all.map(item =>
+                    <tr>
+                        <h1>topic:{item.topic}</h1>
+                        <h1>error code:{item.errorcode}</h1>
+                        <h1>url{item.url}</h1>
+                        <h1>site name{item.site}</h1>
+                    </tr>
+
+
+                )}
+            </div>
+        )
+    }
+}
+const putPropsOnRedux = (reduxStore) => ({
+    all: reduxStore.getItem
+})
+export default withRouter(connect(putPropsOnRedux)(allResult));
+
